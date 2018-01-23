531fcfe3e88c2c1ed2d3c2e5105c1ad72be507f3
Yuya Takashina <takashina2051@gmail.com>
2018-01-23 18:45:26 +0900

    rename project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='depynd',
+      version='0.1',
+      description='Evaluating dependencies among random variables.',
+      author='Yuya Takashina',
+      author_email='takashina2051@gmail.com',
+      packages=['depynd'],
+     )
