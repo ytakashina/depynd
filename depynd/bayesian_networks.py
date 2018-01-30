14677b9b31f25ce47821986f8ff94ff0faf5b858
Yuya Takashina <takashina2051@gmail.com>
2018-01-30 10:47:46 +0900

    Add: stub methods for structure learning for bn & mn

---
+++ depynd/bayesian_networks.py
@@ -0,0 +1,8 @@
+import numpy as np
+from . import mi
+
+
+def iamb(X, lamb=0.0, method=None, options=None):
+    n, d = X.shape
+    edges = []
+    return edges
---
+++ depynd/markov_networks.py
@@ -0,0 +1,8 @@
+import numpy as np
+from . import mi
+
+
+def gsmn(X, lamb=0.0, method=None, options=None):
+    n, d = X.shape
+    edges = []
+    return edges
