8905a4d80979ae12e50f7c451e53c1684526c940
Yuya Takashina <takashina2051@gmail.com>
2018-02-01 15:08:11 +0900

    Add: comments etc

--- depynd/bayesian_networks.py
+++ depynd/bayesian_networks.py
@@ -15,14 +15,28 @@ def is_complete(adj):
 
 
 def iamb(X, lamb=0.0, method=None, options=None):
+    """Search Markov blanket in a Bayesian network.
+    Parameters
+    ----------
+    X : array, shape (n_samples, d)
+        Variable.
+    method: str, default 'knn'
+        Method for MI estimation.
+    options : dict, default None
+        Optional parameters for MI estimation.
+    Returns
+    -------
+    mb : list of list
+        Estimated Markov blanket.
+    """
     n, d = X.shape
-    adj = [[] for i in range(d)]
+    mb = [[] for i in range(d)]
     while True:
         max_cmi = -np.inf
         for i in range(d):
-            non_mb = set(range(d)) - set(adj[i]) - set([i])
+            non_mb = set(range(d)) - set(mb[i]) - set([i])
             x = X[:, [i]]
-            z = X[:, adj[i]]
+            z = X[:, mb[i]]
             for j in non_mb:
                 y = X[:, [j]]
                 cmi = conditional_mutual_information(x, y, z, method, options)
@@ -30,19 +44,68 @@ def iamb(X, lamb=0.0, method=None, options=None):
                     max_cmi = cmi
                     max_pair = i, j
 
-        if max_cmi <= lamb or is_complete(adj):
+        if max_cmi <= lamb or is_complete(mb):
             break
 
-        adj[max_pair[0]] += [max_pair[1]]
+        mb[max_pair[0]] += [max_pair[1]]
 
     for i in range(d):
         x = X[:, [i]]
-        for j in adj[i]:
-            other_mb = list(set(adj[i]) - set([j]))
+        for j in mb[i]:
+            other_mb = list(set(mb[i]) - set([j]))
             y = X[:, [j]]
             z = X[:, other_mb]
             cmi = conditional_mutual_information(x, y, z, method, options)
             if cmi <= lamb:
-                adj[i].remove(j)
+                mb[i].remove(j)
 
+    return mb
+
+
+def mmpc(X, lamb=0.0, method=None, options=None):
+    """Search parents and children in a Bayesian network.
+    Parameters
+    ----------
+    X : array, shape (n_samples, d)
+        Variable.
+    method: str, default 'knn'
+        Method for MI estimation.
+    options : dict, default None
+        Optional parameters for MI estimation.
+    Returns
+    -------
+    pc : list of list
+        Estimated parents and children.
+    """
+    n, d = X.shape
+    pc = [[] for i in range(d)]
+    for i in range(d):
+        x = X[:, [i]]
+        non_pc = set(range(d)) - set(pc[i]) - set([i])
+        for j in non_pc:
+            y = X[:, [j]]
+            z = X[:, pc[i]]
+            cmi = conditional_mutual_information(x, y, z, method=method, options=options)
+            if cmi > lamb:
+                pc[i] += [j]
+
+        for j in pc[i]:
+            other_pc = list(set(pc[i]) - set([j]))
+            y = X[:, [j]]
+            z = X[:, other_pc]
+            cmi = conditional_mutual_information(x, y, z, method=method, options=options)
+            if cmi < lamb:
+                pc[i].remove(j)
+
+    for i in range(d):
+        for j in pc[i]:
+            if not i in pc[j]:
+                pc[i].remove(j)
+
+    return pc
+
+
+def mmhc(X, lamb=0.0, method=None, options=None):
+    adj = [[] for i in range(d)]
+    pc = mmpc(X, lamb, method, options)
     return adj
