82240189bc8a8df3b7b25817c4f0a2662c0a4b8f
Yuya Takashina <takashina2051@gmail.com>
2018-02-03 17:36:46 +0900

    Refactor: made mutual_information._mi_knn() faster.

--- depynd/mutual_information.py
+++ depynd/mutual_information.py
@@ -11,8 +11,8 @@ def _mi_knn(X, Y, k):
     _, d_y = Y.shape
     distances_x = np.linalg.norm(X - X.reshape([n, -1, d_x]), axis=2)
     distances_y = np.linalg.norm(Y - Y.reshape([n, -1, d_y]), axis=2)
-    distances = np.max([distances_x, distances_y], axis=0)
-    epsilons = np.sort(distances, axis=1)[:, k]
+    distances = np.maximum(distances_x, distances_y)
+    epsilons = np.partition(distances, k, axis=1)[:, k]
     idx_discrete = np.isclose(epsilons, 0)
     ks = np.repeat(k, n)
     ks[idx_discrete] = np.sum(np.isclose(
