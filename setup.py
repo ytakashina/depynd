da8b8b4cd83792db1ea172739d0ea0ce088770d7
Yuya Takashina <takashina2051@gmail.com>
2018-02-07 17:48:48 +0900

    v0.2.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='depynd',
-      version='0.2.3',
+      version='0.2.4',
       description='Evaluating dependencies among random variables.',
       author='Yuya Takashina',
       author_email='takashina2051@gmail.com',
