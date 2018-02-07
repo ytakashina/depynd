cd94f255806c848f72746c8af6f5ba6e41c6749c
Yuya Takashina <takashina2051@gmail.com>
2018-02-07 15:49:12 +0900

    v0.2.1
    removed dependency for Cython

--- depynd/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-    name='bayesian_networks',
-    ext_modules=cythonize("bayesian_networks.pyx"),
-)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='depynd',
-      version='0.2.0',
+      version='0.2.1',
       description='Evaluating dependencies among random variables.',
       author='Yuya Takashina',
       author_email='takashina2051@gmail.com',
