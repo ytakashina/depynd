60f2efeb7afe070cc79308edc51c994baaac26e9
Yuya Takashina <takashina2051@gmail.com>
2018-02-07 17:59:48 +0900

    Modified: use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='depynd',
       version='0.2.4',
       description='Evaluating dependencies among random variables.',
       author='Yuya Takashina',
       author_email='takashina2051@gmail.com',
-      packages=['depynd',
-                'depynd.bayesian_networks',
-                'depynd.feature_selection',
-                'depynd.markov_networks',
-                'depynd.mutual_information' ,],
+      packages=find_packages(),
       install_requires=[
           'numpy', 'scipy', 'sklearn',
       ],
