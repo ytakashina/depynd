bc3ad125fd6cb1193eb09e2edbd5e143fbdbfb74
ytakashina <takashina2051@gmail.com>
2018-05-05 14:33:11 +0900

    moved bayesian_networks.iamb to markov_networks.iamb

--- depynd/bayesian_networks/__init__.py
+++ depynd/bayesian_networks/__init__.py
@@ -1,4 +1,4 @@
-from depynd.bayesian_networks.sparse_candidate_ import iamb, mmpc
+from depynd.bayesian_networks.sparse_candidate_ import mmpc
 from depynd.bayesian_networks.mmhc_ import mmhc
 
-__all__ = ['iamb', 'mmpc', 'mmhc']
+__all__ = ['mmpc', 'mmhc']
--- depynd/markov_networks/__init__.py
+++ depynd/markov_networks/__init__.py
@@ -1,5 +1,6 @@
 from depynd.markov_networks.skeptic_ import skeptic
 from depynd.markov_networks.gsmn_ import gsmn
+from depynd.markov_networks.iamb_ import iamb
 from depynd.markov_networks.select_ import stars, select
 
-__all__ = ['skeptic', 'gsmn', 'stars', 'select']
+__all__ = ['skeptic', 'gsmn', 'iamb', 'stars', 'select']
