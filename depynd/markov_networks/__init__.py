cb8e3757eae0301fc28f3a93f9ae247efad5da64
ytakashina <takashina2051@gmail.com>
2018-05-05 15:27:57 +0900

    Modified markov_networks.__init__.py

--- depynd/markov_networks/__init__.py
+++ depynd/markov_networks/__init__.py
@@ -1,6 +1,8 @@
 from depynd.markov_networks.skeptic_ import skeptic
 from depynd.markov_networks.gsmn_ import gsmn
 from depynd.markov_networks.iamb_ import iamb
-from depynd.markov_networks.select_ import stars, select
+from depynd.markov_networks.gplm_ import fgplm
+from depynd.markov_networks.stars_ import stars
+from depynd.markov_networks.select_ import select
 
-__all__ = ['skeptic', 'gsmn', 'iamb', 'stars', 'select']
+__all__ = ['skeptic', 'gsmn', 'iamb', 'stars', 'select', 'fgplm']
