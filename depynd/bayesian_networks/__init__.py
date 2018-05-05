142581e8ade9bc40a3b760867a0c5d8797dc5ce4
ytakashina <takashina2051@gmail.com>
2018-05-05 14:34:17 +0900

    Renamed bayesian_networks.sparse_candidate -> bayesian_networks.mmpc

--- depynd/bayesian_networks/__init__.py
+++ depynd/bayesian_networks/__init__.py
@@ -1,4 +1,4 @@
-from depynd.bayesian_networks.sparse_candidate_ import mmpc
+from depynd.bayesian_networks.mmpc_ import mmpc
 from depynd.bayesian_networks.mmhc_ import mmhc
 
 __all__ = ['mmpc', 'mmhc']
