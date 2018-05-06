c4b4dca739df4a3b81db44d0f27a2ed1018394f7
ytakashina <takashina2051@gmail.com>
2018-05-06 21:13:12 +0900

    Renamed mb -> adj in markov_networks.gplm()

--- depynd/markov_networks/gplm_.py
+++ depynd/markov_networks/gplm_.py
@@ -5,14 +5,14 @@ from depynd.information import conditional_mutual_information
 
 def gplm(X, lamb=0.0, method=None, options=None):
     n, d = X.shape
-    mb = np.zeros([d, d], dtype=bool)
+    adj = np.zeros([d, d], dtype=bool)
     while True:
         vmax = -np.inf
         for i in range(d):
             x = X[:, [i]]
-            z = X[:, mb[i]]
-            non_mb = ~mb[i] & (np.arange(d) != i)
-            for j in non_mb.nonzero()[0]:
+            z = X[:, adj[i]]
+            non_adj = ~adj[i] & (np.arange(d) != i)
+            for j in non_adj.nonzero()[0]:
                 if i == j:
                     continue
                 y = X[:, [j]]
@@ -24,28 +24,28 @@ def gplm(X, lamb=0.0, method=None, options=None):
         if vmax <= lamb:
             break
 
-        mb[imax, jmax] = mb[jmax, imax] = 1
+        adj[imax, jmax] = adj[jmax, imax] = 1
 
-        if np.count_nonzero(mb) == d * (d - 1) / 2:
+        if np.count_nonzero(adj) == d * (d - 1) / 2:
             break
 
     while True:
         vmin = np.inf
         for i in range(d):
             x = X[:, [i]]
-            for j in mb[i].nonzero()[0]:
-                other_mb = mb[i] & (np.arange(d) != j)
+            for j in adj[i].nonzero()[0]:
+                other_adj = adj[i] & (np.arange(d) != j)
                 y = X[:, [j]]
-                z = X[:, other_mb]
+                z = X[:, other_adj]
                 cmi = conditional_mutual_information(x, y, z, method, options)
                 if vmin > cmi:
                     vmin = cmi
                     imin, jmin = i, j
 
         if vmin >= lamb:
-            return mb
+            return adj
 
-        mb[imin, jmin] = mb[jmin, imin] = 0
+        adj[imin, jmin] = adj[jmin, imin] = 0
 
-        if np.count_nonzero(mb) == 0:
-            return mb
+        if np.count_nonzero(adj) == 0:
+            return adj
