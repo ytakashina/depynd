b2d4ae78838de2afcdfa529c07505a7e65fd2d96
ytakashina <takashina2051@gmail.com>
2018-06-02 05:33:16 +0900

    Added bayesian_networks

--- depynd/__init__.py
+++ depynd/__init__.py
@@ -1,10 +1,12 @@
 from depynd import (
+    bayesian_networks,
     information,
     feature_selection,
     markov_networks,
 )
 
 __all__ = [
+    'bayesian_networks',
     'information',
     'feature_selection',
     'markov_networks',
---
+++ depynd/bayesian_networks/__init__.py
@@ -0,0 +1,4 @@
+from depynd.bayesian_networks._mmpc import mmpc
+from depynd.bayesian_networks._mmhc import mmhc
+
+__all__ = ['mmpc', 'mmhc']
