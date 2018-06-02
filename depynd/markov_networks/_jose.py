6a8a0a37a7cedcb2e04c24e557677dc0a12ff8e9
ytakashina <takashina2051@gmail.com>
2018-06-02 18:55:31 +0900

    Removed trial code from depynd.markov_networks

--- depynd/markov_networks/_jose.py
+++ depynd/markov_networks/_jose.py
@@ -47,35 +47,3 @@ def jose(X, lamb=0.01, tol=1e-6, max_iter=100):
     adj = np.abs(theta) > 1e-6
     adj[np.eye(d, dtype=bool)] = 0
     return adj
-
-
-if __name__ == '__main__':
-    adj0 = np.zeros([9, 9])
-    for i in range(3, 9):
-        adj0[i, i - 3] = adj0[i - 3, i] = 1
-    adj0[0, 1] = adj0[1, 0] = 1
-    adj0[1, 2] = adj0[2, 1] = 1
-    adj0[3, 4] = adj0[4, 3] = 1
-    adj0[4, 5] = adj0[5, 4] = 1
-    adj0[6, 7] = adj0[7, 6] = 1
-    adj0[7, 8] = adj0[8, 7] = 1
-
-
-    def sample(adj, x, i, size=1):
-        p = 1 / (1 + np.exp(-adj[i] @ x))
-        sample = multinomial(1, [p, 1 - p]).rvs(size)
-        return sample.argmax()
-
-
-    def gibbs_sampling(adj, L=100):
-        X = []
-        x = np.ones(9, dtype=int)
-        for t in range(L):
-            for i in range(9):
-                x[i] = sample(adj, x, i)
-            X.append(np.copy(x))
-        return np.vstack(X)
-
-
-    X = gibbs_sampling(adj0)
-    adj = jose(X, 4.53999298e-05)
