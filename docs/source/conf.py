5e4d22d36d6e804c8a02bc69550908c02897545a
ytakashina <takashina2051@gmail.com>
2018-06-02 23:20:56 +0900

    Removed numpydoc dependency for documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # ones.
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'numpydoc',
+    # 'numpydoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
