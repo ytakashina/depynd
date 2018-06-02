5d2fbc6f7708461654e2a6d9ff74b12bfecf0d68
ytakashina <takashina2051@gmail.com>
2018-06-02 19:21:43 +0900

    Added long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+long_description = """`depynd` is a Python library for learning dependencies between random variables from data.
+ It supports
+- Mutual information (MI) estimation for discrete-continuous mixtures
+- Feature selection algorithms based on MI
+- Markov random field structure learning algorithms
+"""
+
 setup(
     name='depynd',
     version='0.4.4',
     description='Evaluating dependencies among random variables.',
+    long_description=long_description,
     author='Yuya Takashina',
     author_email='takashina2051@gmail.com',
     packages=find_packages(),
