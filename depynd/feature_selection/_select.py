2cebe333e898493600353672ca3e8a023502b999
ytakashina <takashina2051@gmail.com>
2018-06-04 02:36:43 +0900

    Modified docstrings

--- depynd/feature_selection/_select.py
+++ depynd/feature_selection/_select.py
@@ -4,23 +4,23 @@ from depynd.feature_selection import mrmr, mifs
 
 
 def select(X, y, lamb=0.0, method='mifs', **kwargs):
-    """Select effective features in ``X`` on predinting ``y``.
+    """Select effective features in ``X`` on predicting ``y``.
 
     Parameters
     ----------
     X : array-like, shape (n_samples, d)
-        The observations of feature variables.
+        Observations of feature variables.
     y : array-like, shape (n_samples)
-        The observations of the target variable.
+        Observations of the target variable.
     lamb: float, default 0.0
-        The threshold for independence tests.
+        Threshold for independence tests.
     method: {'mifs', 'mrmr'}, default 'mifs'
-        The method for feature selection. Either 'mifs' or 'mrmr' can be used.
+        Feature selection method.
 
     Returns
     -------
     indices : list
-        The indices of the selected features.
+        Indices of the selected features.
     """
     X, y = check_X_y(X, y)
     if method == 'mifs':
--- depynd/information/_dr.py
+++ depynd/information/_dr.py
@@ -10,20 +10,20 @@ def mi_dr(X, Y, sigma, n_bases, maxiter):
     Parameters
     ----------
     X : array-like, shape (n_samples, d_x) or (n_samples)
-        The observations of a variable.
+        Observations of a variable.
     Y : array-like, shape (n_samples, d_y) or (n_samples)
-        The observations of the other variable.
+        Observations of the other variable.
     sigma : float
-        The kernel width for density ratio estimator.
+        Kernel width for density ratio estimator.
     n_bases : int
-        The number of bases used in density ratio estimation.
+        Number of bases used in density ratio estimation.
     maxiter : int
-        The maximum number of iteration in density ratio estimation.
+        Maximum number of iteration in density ratio estimation.
 
     Returns
     -------
     mi : float
-        The estimated mutual information between X and Y.
+        Estimated mutual information between ``X`` and ``Y``.
     """
     n, d_x = X.shape
     _, d_y = Y.shape
