c0c6f7f0a5a72221df7b6739b1d14f8799267c7c
ytakashina <takashina2051@gmail.com>
2018-06-03 15:53:46 +0900

    Removed mimat & cmimat from information module

--- depynd/information/__init__.py
+++ depynd/information/__init__.py
@@ -1,5 +1,5 @@
-from depynd.information._dr import mi_dr
-from depynd.information._knn import mi_knn
-from depynd.information._information import mutual_information, conditional_mutual_information, mimat, cmimat
+from ._dr import mi_dr
+from ._knn import mi_knn
+from ._information import mutual_information, conditional_mutual_information
 
-__all__ = ['mi_dr', 'mi_knn', 'mutual_information', 'conditional_mutual_information', 'mimat', 'cmimat']
+__all__ = ['mi_dr', 'mi_knn', 'mutual_information', 'conditional_mutual_information']
