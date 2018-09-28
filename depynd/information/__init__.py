99b934954ca6fdd3ad657183f86a45f812014954
ytakashina <takashina2051@gmail.com>
2018-09-28 15:44:26 +0900

    Added plug-in method for MI estimation

--- depynd/information/__init__.py
+++ depynd/information/__init__.py
@@ -1,5 +1,6 @@
 from ._dr import _mi_dr
 from ._knn import _mi_knn
+from ._plugin import _mi_plugin
 from ._information import mutual_information, conditional_mutual_information
 
-__all__ = ['_mi_dr', '_mi_knn', 'mutual_information', 'conditional_mutual_information']
+__all__ = ['_mi_dr', '_mi_knn', '_mi_plugin', 'mutual_information', 'conditional_mutual_information']
