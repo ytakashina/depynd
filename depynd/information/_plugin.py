d235f0d4fdef15e23350ce9fac5379b641f39406
ytakashina <takashina2051@gmail.com>
2018-10-02 14:47:58 +0900

    Renamed _h_plugin to _entropy_plugin

--- depynd/information/_plugin.py
+++ depynd/information/_plugin.py
@@ -3,10 +3,10 @@ import numpy as np
 
 def _mi_plugin(X, Y):
     XY = np.hstack([X, Y])
-    return _h_plugin(X) + _h_plugin(Y) - _h_plugin(XY)
+    return _entropy_plugin(X) + _entropy_plugin(Y) - _entropy_plugin(XY)
 
 
-def _h_plugin(X):
+def _entropy_plugin(X):
     n, d = X.shape
     _, cnt = np.unique(X, axis=0, return_counts=True)
     p = cnt / n
