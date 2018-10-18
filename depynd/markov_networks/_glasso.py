2082a19279f025c85303ea989814683948b12a7a
ytakashina <takashina2051@gmail.com>
2018-10-18 17:50:57 +0900

    pass only eligible arguments from kwargs to graph_lasso in markov_networks

--- depynd/markov_networks/_glasso.py
+++ depynd/markov_networks/_glasso.py
@@ -23,6 +23,7 @@ def _glasso(X, lamb, return_precision=False, **kwargs):
         Estimated adjacency matrix (or precision matrix if ``return_precision`` is True) of an MRF.
     """
     cov = np.cov(scale(X), rowvar=False)
+    kwargs = {k: v for k, v in kwargs.items() if k in graph_lasso.__code__.co_varnames}
     pre = graph_lasso(cov, alpha=lamb, **kwargs)[1]
     if return_precision:
         return pre
